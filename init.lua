b0c8ada5f96dcebeaede953c959c4455d7b34363
Peyman <hassani.peyman627@gmail.com>
2025-06-12 16:38:24 +0330

    Add gopls and pylsp

--- init.lua
+++ init.lua
@@ -680,7 +680,8 @@ require('lazy').setup({
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
         -- clangd = {},
-        -- gopls = {},
+        gopls = {},
+        pylsp = {},
         -- pyright = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
