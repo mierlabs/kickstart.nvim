ba52c10629fe33bcbb705c6302d138186d1621c1
Peyman <hassani.peyman627@gmail.com>
2025-06-27 18:01:07 +0330

    Add harpoon2

--- init.lua
+++ init.lua
@@ -532,6 +532,38 @@ require('lazy').setup({
     end,
   },
 
+  {
+    'ThePrimeagen/harpoon',
+    branch = 'harpoon2',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    config = function()
+      local harpoon = require 'harpoon'
+
+      -- required
+      harpoon:setup()
+
+      vim.keymap.set('n', '<leader>a', function()
+        harpoon:list():add()
+      end)
+      vim.keymap.set('n', '<leader>w', function()
+        harpoon.ui:toggle_quick_menu(harpoon:list())
+      end)
+
+      vim.keymap.set('n', '<leader>j', function()
+        harpoon:list():select(1)
+      end)
+      vim.keymap.set('n', '<leader>k', function()
+        harpoon:list():select(2)
+      end)
+      vim.keymap.set('n', '<leader>l', function()
+        harpoon:list():select(3)
+      end)
+      vim.keymap.set('n', '<leader>;', function()
+        harpoon:list():select(4)
+      end)
+    end,
+  },
+
   -- LSP Plugins
   {
     -- `lazydev` configures Lua LSP for your Neovim config, runtime and plugins
