109bebc32246a7ce7be0e4eff829c5f210b3f231
Peyman <hassani.peyman627@gmail.com>
2025-07-06 19:44:50 +0330

    Commet jj as escape to check if caps lock can be better

--- init.lua
+++ init.lua
@@ -215,7 +215,7 @@ vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper win
 -- vim.keymap.set("n", "<C-S-k>", "<C-w>K", { desc = "Move window to the upper" })
 
 -- personal keymaps
-vim.keymap.set('i', 'jj', '<ESC>', { desc = 'exit insert mode' })
+-- vim.keymap.set('i', 'jj', '<ESC>', { desc = 'exit insert mode' })
 vim.keymap.set('x', '<leader>p', [["_dP]])
 -- vim.keymap.set('n', '<CR>', 'o<ESC>', { noremap = true, silent = true })
 vim.keymap.set('n', 'gh', '<C-o>', { desc = 'Jump back in jumplist' })
